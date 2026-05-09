c3ca3cf8e36e7b172cbd3a1ba50174c1614d7c8b
Priyanshu Rajput <priyanshurajput246@gmail.com>
2026-05-09 20:21:26 +0530

    util code discussion

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 import mongoose from 'mongoose';
-import express from 'express'
 import dotenv from 'dotenv';
+import { app } from './app.js';
 import connectDB from './bd/db.js';
 dotenv.config();
 
-const app = express();
+
 const PORT = process.env.PORT || 8080;
-connectDB();
-app.get("/",(req,res) =>{
-    res.send("Hello world");
+connectDB()
+.then(() =>{
+    app.listen(PORT,()=> {
+        console.log(`Server is running on PORT`,PORT);
+    })
+})
+.catch((err)=>{
+    console.log("ERROR MONGODB connection failed:",err)
 })
 
-app.listen(PORT,()=>{
-    console.log("Server is connected on PORT : "+PORT);
-})
 
 
